0b999fb144ccaa215ee00d16bd72953c35480142
Jon Church <me@jonchurch.com>
2016-06-20 20:22:21 -0400

    added seconds countdown, added zeroes before single digit seconds

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,23 @@
-console.log('My JavaScript is working...!!!!');
+$(document).ready(function() {
+    var start = $('#start');
+    var minutes = $('#minutes');
+    var seconds = $('#seconds');
+    start.on('click', startCountdown);
+
+    function startCountdown() {
+        setInterval(function() {
+            var secondsVal = +seconds.text(); //the plus uses type coercion to make seconds.text a number if it can
+            if (secondsVal === 0) {
+                seconds.text(59);
+            } else {
+                if (secondsVal <= 10) {
+                    console.log(secondsVal);
+                    seconds.text("0" + (secondsVal - 1));
+                } else {
+                    seconds.text(secondsVal - 1);
+                }
+            }
+
+        }, 1000);
+    }
+});
